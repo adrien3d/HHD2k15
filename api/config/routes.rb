8e85b893bbc5698c537e492c9fb2ea4afa8db05d
Maxence Henneron <dernise@live.fr>
2015-10-09 23:36:44 +0200

    Projet propre

---
+++ api/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  namespace :api, :defaults => {format: 'json'} do
+    namespace :v1 do
+      post '/auth' => 'user#get_token'
+      resources :user, except: [:index, :delete, :new]
+    end
+  end
+end
