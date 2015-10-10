0f6876d48e9446cbf453d10a0db04da0608e42bb
Adrien CHAPELET <adrien3d@gmail.com>
2015-10-10 02:00:43 +0200

    Prep requests

--- appli/tabs/www/js/app.js
+++ appli/tabs/www/js/app.js
@@ -100,6 +100,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       }
     }
   });
+/*
+  .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html'
+      controller: 'LoginCtrl'
+  })*/
 
   // if none of the above states are matched, use this as the fallback
 
--- appli/tabs/www/js/services.js
+++ appli/tabs/www/js/services.js
@@ -1,6 +1,6 @@
-angular.module('starter.services', [])
+var app = angular.module('starter.services', [])
 
-.factory('Chats', function() {
+app.factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
@@ -53,3 +53,68 @@ angular.module('starter.services', [])
     }
   };
 });
+
+
+app.factory('FriendsNearby', function() {
+  var friends = [{
+    id: 0,
+    name: 'Maxence Henneron',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Elie Alawoe',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Adrien Chapelet',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    getFromApi: function() {
+      /*return $http.post("https://www.yoursite.com/users").then(function(response){
+        users = response;
+        return users;
+      });*/
+    },
+    get: function(friendId) {
+      for (var i = 0; i < friends.length; i++) {
+        if (friends[i].id === parseInt(friendId)) {
+          return friends[i];
+        }
+      }
+      return null;
+    }
+  };
+});
+/*
+.service('LoginService', function($q) {
+    return {
+        loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+            if (name == 'user' && pw == 'secret') {
+                deferred.resolve('Welcome ' + name + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+});
+*/
