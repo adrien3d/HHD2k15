4f9a5aa07132b43f80c8e1c984e40106365a88bb
Adrien CHAPELET <adrien3d@gmail.com>
2015-10-10 20:26:00 +0200

    Changement texte chat

--- appli/tabs/www/js/services.js
+++ appli/tabs/www/js/services.js
@@ -5,36 +5,21 @@ app.factory('Chats', function() {
 
   // Some fake testing data
   var chats = [{
-    id: 0,
-    name: 'Maxence Henneron',
-    lastText: 'You on your way?',
-    face: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/005/073/292/1632362.jpg'
-  }, {
     id: 1,
     name: 'Elie Alawoe',
-    lastText: 'Hey, it\'s me',
+    lastText: "Ben alors, on attend pas Élie ?",
     face: 'https://i.vimeocdn.com/portrait/8889425_300x300.jpg'
   }, {
+    id: 0,
+    name: 'Maxence Henneron',
+    lastText: "Un p'tit verre à la plage ?",
+    face: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/005/073/292/1632362.jpg'
+  }, {
     id: 2,
-    name: 'Adrien Chapelet',
-    lastText: 'I should buy a boat',
-    face: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/005/062/3d9/3598455.jpg'
-  }, {
-    id: 3,
     name: 'Sebastien Cockedey',
-    lastText: 'Look at my mukluks!',
+    lastText: 'Look at my mukluks !',
     face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Sebastien Merchez',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }, {
-    id: 5,
-    name: 'Xavier Chevalier',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+  },];
 
   return {
     all: function() {
