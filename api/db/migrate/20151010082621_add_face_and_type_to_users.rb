59c1e9b338b6774b77b274b914fcf3669f7fe518
Maxence Henneron <dernise@live.fr>
2015-10-10 10:32:56 +0200

    Modification d'un champ en scred

--- api/db/migrate/20151010082621_add_face_and_type_to_users.rb
+++ api/db/migrate/20151010082621_add_face_and_type_to_users.rb
@@ -1,6 +1,6 @@
 class AddFaceAndTypeToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :type, :integer, default: 0
+    add_column :users, :user_status, :integer, default: 0
     add_column :users, :face, :string, default: 'http://www.clker.com/cliparts/5/7/4/8/13099629981030824019profile.svg.med.png'
   end
 end
