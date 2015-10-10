a1b635a8eed9beba165edc9c710e9bd26f874663
Maxence Henneron <dernise@live.fr>
2015-10-10 16:26:25 +0200

    Fixed scope

--- api/app/models/invite.rb
+++ api/app/models/invite.rb
@@ -1,8 +1,7 @@
 class Invite < ActiveRecord::Base
   belongs_to :user
 
-  validates_uniqueness_of :user_id, :scope => [:friend_id]
-
+  validates_uniqueness_of :user_id, scope: :friend_id
 
   def user
 
